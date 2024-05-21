94e1552a65f233382c8164771c2d5980b40fe978
gizollo <165858351+gizollo@users.noreply.github.com>
2024-05-20 21:40:10 -0300

    Update tela_conteudo.php

--- tela_conteudo.php
+++ tela_conteudo.php
@@ -5,10 +5,63 @@
 </head>
 <body>
     <h1>Bem-vindo, <?php echo $_COOKIE["user"]; ?>!</h1>
-    <p>Aqui você encontra o conteúdo exclusivo para você.</p>
+    <p>Dama do Lago 
+
+Eis-me a caminhar no céu noturno <br> 
+Em frente a um lago profundo <br>
+Me deparo com uma bela dama <br>
+E de fato uma vista belíssima <br>
+Seu corpo esguio, à luz prateada da noite <br> 
+Seu olhar nas páginas do livro, mergulhava com entusiasmo <br>
+Nas águas escuras do lago, o reflexo brilhava <br>
+Refletia uma certa pureza, cabelos castanhos, como rios em correnteza <br>
+Minha surpresa não foi sua feição plena e bela <br>
+Foi seu fascínio durante sua leitura <br>
+Ela segurava um livro com mãos graciosas <br>
+Páginas cheias de histórias misteriosas <br>
+Aproximei-me e indaguei se poderias acender meu tabaco <br>
+E com apenas um gesto simples, sorriu e acenou <br>
+Ó bela dama meu coração foi tomado por ti <br>
+O luar acariciava seu rosto sereno <br>
+Sua face, um retrato de graça e encanto <br>
+Vislumbrei seus castanhos olhos como de mel <br>
+O céu estrelado e uma paisagem deslumbrante <br>
+E tu segurastes um livro mais robusto que toras <br>
+Mais volumoso que o lago a vossa volta <br>
+Quando quase acabastes meu tabaco <br>
+Ouvira tua voz a cada palavra que lia eras uma canção <br> 
+Que ecoava no meus ouvidos e pensamentos <br>
+Era como um raio luminoso no breu <br>
+Falou de linhas da Historia, fiquei preso e hipnotizado. <br>
+E minha bela dama à de partir para seu recanto <br>
+Me deixando deveras em pranto e espanto <br>
+Voltara na outra noite e continuaste sua leitura <br> 
+E mesmo não sabendo estavas bela mais uma vez <br>
+Ainda lhe deslumbrava de longe <br>
+Olhaste-me com surpresa de novo <br>
+E seus olhos voltados ao livro <br>
+Enquanto arrumava seus cabelos <br>
+Delicada e graciosamente <br>
+Nossos olhos se cruzam com frequência <br> 
+E em sua companhia me mostra um mundo além do momento. <br>
+Após seu termino de leitura, levantaras <br>
+E o único som que ouvia eras o silencio <br>
+Uma visão de esperança e um encanto tão terno <br>
+Acostumei lhe ver durante as noites de luar <br>
+Sempre lhe esperando à fumar <br>
+Em uma noite tu desapareceras <br>
+A sensação de não lhe ver me deixa aflito <br>
+Ó bela dama, porque me abandonastes ? <br>
+Deixaras seu único contemplador perdido <br>
+Nunca mais pude lhe ver novamente <br>
+Suas marcas me deixam cicatrizes em meu peito <br> 
+Mas nunca me esquecera de seus encantos <br>
+Nunca soubeste seu nome <br>
+Mas para mim sempre serás minha beladama do lago.</p>
 </body>
-
-
 </html>
+<!-- Código não finalizado -->
+
+
 
 
