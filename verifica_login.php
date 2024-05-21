7e9177d069ee47caacbe77055a37e1cee765ad06
gizollo <165858351+gizollo@users.noreply.github.com>
2024-05-20 22:14:45 -0300

    Update verifica_login.php

--- verifica_login.php
+++ verifica_login.php
@@ -1,52 +1,41 @@
 <?php
-//código não finalizado
+
+$usuarios = [
+    [
+        'nome' => 'Zollo',
+        'email' => 'giovanni@zollo',
+        'senha' => password_hash('senha123', PASSWORD_DEFAULT)
+    ],
+   
+];
+
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
-
-
-// Conexão com o banco de dados
-$conn = new mysqli("localhost", "usuario", "senha", "database_name");
-
-$usuario = "zollo";
-$senha = "senha123";
-
-// Verificação de conexão
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Consulta no banco de dados
-$query = "SELECT * FROM usuarios WHERE email = ?";
-$stmt = $conn->prepare($query);
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$result = $stmt->get_result();
-
-// Se o usuário existir, cria uma sessão e um cookie
-if ($result->num_rows > 0) {
-    $usuario = $result->fetch_assoc();
-    session_start();
-
-    // Verifica a senha fornecida com a senha armazenada no banco de dados
-    if (password_verify($senha, $usuario['senha'])) {
-        setcookie("usuario", $usuario['nome'], time() + (86400 * 7), "/"); // 86400 s = 1 dia por 7 dias
-
-        // Armazena os dados do usuário em uma variável de sessão
-        $_SESSION['usuario'] = $usuario;
-
-    
-        header("location: tela_conteudo.php");
-    } else {
-        
-        header("location: tela_login.php?error=1");
+// Verifica se o usuário existe
+$usuario = null;
+foreach ($usuarios as $u) {
+    if ($u['email'] === $email) {
+        $usuario = $u;
+        break;
     }
-} else {
-
-    header("location: tela_login.php?error=1");
 }
 
-$conn->close();
+if ($usuario && password_verify($senha, $usuario['senha'])) {
+    
+    // Cria uma sessão e um cookie
+    session_start();
+    setcookie("usuario", $usuario['nome'], time() + (86400 * 7), "/"); // 86400 s = 1 dia por 7 dias
+
+    // Armazena os dados do usuário em uma variável de sessão
+    $_SESSION['usuario'] = $usuario;
+
+    header("location: tela_conteudo.php");
+} 
+
+else {
+   header("location: error.php");
+}
 
 //sites que utilizam sistemas de login em PHP com sessões e cookies:
 
